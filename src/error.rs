91b071f3bac29fa73a6b693a6b839b91a33fcba3
aragami3070 <smirnov17612@gmail.com>
2026-04-17 22:38:14 +0400

    feat(error): impl from CloudError for String

--- src/error.rs
+++ src/error.rs
@@ -1,10 +1,24 @@
+use reqwest::StatusCode;
 use thiserror::Error;
 
+use crate::json_result::to_err;
+
 #[derive(Error, Debug)]
-pub enum CloudError {
+pub enum CloudeError {
     #[error("Reqwest error: {0}")]
     ReqwestError(#[from] reqwest::Error),
 
-    #[error("Rclone error: {0}")]
-    RcloneError(String),
+    #[error("Rclone error")]
+    RcloneError((StatusCode, String)),
+}
+
+impl From<CloudeError> for String {
+    fn from(value: CloudeError) -> Self {
+        match value {
+            CloudeError::ReqwestError(err) => {
+                to_err(StatusCode::INTERNAL_SERVER_ERROR, &err.to_string())
+            }
+            CloudeError::RcloneError((status, err)) => to_err(status, &err),
+        }
+    }
 }
