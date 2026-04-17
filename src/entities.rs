770b8f8c4782bd71331317e4b14ca3f5298868f6
aragami3070 <smirnov17612@gmail.com>
2026-04-18 00:57:22 +0400

    feat(entities): rework ListRemoteResponse to RemoteConfig

--- src/entities.rs
+++ src/entities.rs
@@ -2,15 +2,19 @@ use std::collections::HashMap;
 
 use serde::{Deserialize, Serialize};
 
-#[derive(Deserialize)]
-pub struct ListRemotesResponse {
-    pub remotes: Vec<String>,
-}
-
 #[derive(Serialize)]
 pub struct ConfigCreateRequest {
     pub name: String,
     #[serde(rename = "type")]
     pub r_type: String,
     pub parameters: HashMap<String, String>,
+}
+
+#[derive(Debug, Deserialize)]
+pub struct RemoteConfig {
+    #[serde(rename = "type")]
+    pub r#type: String,
+
+    #[serde(flatten)]
+    pub extra: HashMap<String, serde_json::Value>,
 }
