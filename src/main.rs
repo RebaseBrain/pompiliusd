19c28efc2b68ee5011e173c54ebde9da8f7cc0a1
aragami3070 <smirnov17612@gmail.com>
2026-04-17 22:42:57 +0400

    fix(main): correct zbus for refactor changes

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,19 @@
-use clouds_api::RcClone;
+use clouds_api::{Cloude, rclone_api::RcClone};
 use reqwest::Client;
 use std::{error::Error, future::pending};
 use zbus::connection;
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn Error>> {
-    let rclone = RcClone {
-        client: Client::new(),
-        url: String::from("http://127.0.0.1:5572/"),
+    let cloude = Cloude {
+        rclone: RcClone {
+            client: Client::new(),
+            url: String::from("http://127.0.0.1:5572/"),
+        },
     };
     let _conn = connection::Builder::session()?
         .name("org.zbus.cloud_api")?
-        .serve_at("/org/zbus/cloud_api", rclone)?
+        .serve_at("/org/zbus/cloud_api", cloude)?
         .build()
         .await?;
 
